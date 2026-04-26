7ae927119753e317ea0bdbc2fdecd82fe18ba450
Bluescratch <dzadu02@gmail.com>
2026-04-26 22:27:48 +0200

    actuall init

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv("token.env")
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello")
+    
+bot.run(TOKEN)
