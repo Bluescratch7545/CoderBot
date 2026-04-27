30d188f921357344700b2a1eeedd501b90151233
Bluescratch <dzadu02@gmail.com>
2026-04-27 16:24:03 +0200

    Do

--- bot.py
+++ bot.py
@@ -20,4 +20,28 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hello")
     
+    
+DNP_ENABLED = True
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    
+    if message.reference:
+        try:
+            replied_message = await message.channel.fetch_message(message.reference.message_id)
+        except:
+            return
+        
+        replied_user = replied_message.author
+        role_names = [role.name for role in replied_user.roles]
+        
+        if "DoNotPing" in role_names and DNP_ENABLED:
+            await message.channel.send(
+                f"Please do not ping users with the No Ping role!"
+            )
+    
+    await bot.process_commands(message)
+    
 bot.run(TOKEN)
